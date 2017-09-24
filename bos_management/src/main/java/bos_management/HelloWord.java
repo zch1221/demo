a9a28591946ce51b1c5727d3de435628ce298caf
zch <bos@zch.cn>
2017-09-24 23:03:06 +0800

    githubdemo

---
+++ bos_management/src/main/java/bos_management/HelloWord.java
@@ -0,0 +1,12 @@
+package bos_management;
+
+public class HelloWord {
+
+	public static void main(String[] args) {
+		demo();
+	}
+	public static  void demo() {
+		System.out.println("mmm");
+	}
+
+}
